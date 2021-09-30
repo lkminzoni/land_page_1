aaf78e997547fdf4a0d270795c0a475b0016b3f2
lkminzoni <lkminzoni@hotmai.com>
2021-09-30 00:35:41 -0500

    modified 09/30

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,47 @@
-const nextBtn = document.querySelector('#btn');
-nextBtn.addEventListener('click', getValue);
+class EnterForm{
+    constructor(){
+        this.nextBtn = document.querySelector('#btn');
+        this._captured = document.querySelector("#inp");
+        this.paragraph = document.querySelector('#parag');
+        this._password = document.querySelector('#password');
+        this.start();
+    }
+    start(){
+        this.nextBtn.addEventListener('click', this.getUserValue.bind(this));
+    }
+    
+    getUserValue(){
+        let usernameInp = this.capturedUsername.value;
+        this.paragraph.innerText = `Heloo! ${usernameInp.toUpperCase()}`;
+        this.capturedUsername.value = '';
+        this.capturedPassword.value = '';
+        setInterval(() => {
+            return this.paragraph.innerText = '';
+        },2000);
+    }
 
-function getValue(){
-    let captured = document.querySelector("#inp");
-    let passwordInp = document.querySelector('#password');
-    const paragraph = document.querySelector("#parag");paragraph.innerText = `Hello ${captured.value.toUpperCase()}`;
-    captured.value = '';
-    passwordInp.value = '';
-    setTimeout(() => {
-        paragraph.innerText = '';
-    }, 2000);
+    set capturedUsername(value){
+        this._captured = value;
+    }
+
+    get capturedUsername(){
+        return this._captured;
+    }
+
+    set capturedPassword(value){
+        this._password = value;
+    }
+
+    get capturedPassword(){
+        return this._password;
+    }
+
+    
 }
+
+
+const begining = new EnterForm();
+
+
+
 
