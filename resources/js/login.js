ef8fa25c0d93e11a610e76030e74e7765205b4a9
Lucas Minzoni <lkminzoni@hotmail.com>
2022-01-08 21:24:01 -0600

    improve 01/08/22

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,16 +1,15 @@
 class LogInForm{
     constructor(){
-        this.nextBtn = document.querySelector('#btn');
+        
         this._captured = document.querySelector("#inp");
         this.paragraph = document.querySelector('#parag');
         this._password = document.querySelector('#password');
-        this.startLogIn();
-    }
-    startLogIn(){
-        this.nextBtn.addEventListener('click', this.getUserValue.bind(this));
+        this.capturedEmail = document.querySelector('#email');
+        this.capturedUsername = document.querySelector('#inp')
     }
     
-    getUserValue(){
+    
+    getUserValue(e){
         let usernameInp = this.capturedUsername.value;
         if(usernameInp !== ''){
             if(this.capturedPassword.value === '' || this.capturedPassword.value.length < 6){
@@ -31,6 +30,7 @@ class LogInForm{
             this.capturedUsername.value,
             this.capturedPassword.value = '';
         }
+        
     }
 
     msgErrorLogin(itemToCheck,itemToAppend){
@@ -41,6 +41,52 @@ class LogInForm{
         setTimeout(() => {
             itemToAppend.nextElementSibling.remove()
         },3000);
+    }
+
+    oldUser(){
+        if(this.capturedEmail.value.toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )){
+            this.capturedEmail.value = '';
+        }else{
+            this.msgErrorLogin('email',this.capturedEmail)
+            this.capturedEmail.value = '';
+        }
+    }
+
+    newAccount(){
+        if(this.capturedUsername.value === ''){
+            this.msgErrorLogin('Username',this.capturedUsername)
+            // this.capturedFirstName()
+        }
+        if(this._firstName.value === ''){
+            this.msgErrorLogin('First Name',this._firstName)
+            // this.capturedFirstName()
+        }
+        if(this._lastName.value === ''){
+            this.msgErrorLogin('Last Name',this._lastName)
+            // this.capturedLastName()
+        }
+        if(!this.enterEmail.value.toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )){
+            this.msgErrorLogin('Email', this.enterEmail)
+            // this.capturedLastName()
+        }
+        if(this.reEmail.value === '' || this.reEmail.value !== this.enterEmail.value){
+            this.msgErrorLogin('Email',this.reEmail)
+            // this.capturedLastName()
+        }
+        if(this.capturedPassword.value === ''){
+            this.msgErrorLogin('Password',this.capturedPassword)
+            // this.capturedLastName()
+        }
+        if(this.capturedPassword.value !== this.rePass.value || this.rePass.value === ''){
+            this.msgErrorLogin('Password',this.rePass)
+            // this.capturedLastName()
+        }
     }
 
     set capturedUsername(value){
--- resources/js/logon.js
+++ resources/js/logon.js
@@ -4,11 +4,16 @@ class LogonForm extends LogInForm{
         this._firstName = document.querySelector('#fName');
         this._lastName = document.querySelector('#lName');
         this.enterEmail = document.querySelector('#email');
+        this.reEmail = document.querySelector('#reEmail');
         this.rePass = document.querySelector('#rePassword');
+        this.nextBtn = document.querySelector('#btn');
         this.cancelBtn = document.querySelector('#btn-cancel');
         // Logon link
         this.newAccountLink = document.querySelector('#newAccount');
+        this.newAccountBtn = document.querySelector('#btn-newAccount')
         // Forgot password
+        this.btnEmail = document.querySelector('#btn-email');
+        this.enterEmailBtn = document.querySelector('#btn-email');
         this.oldAccountLink = document.querySelector('#oldAccount');
         this.startLogOn();
         // cancel button switch
@@ -19,6 +24,9 @@ class LogonForm extends LogInForm{
         this.newAccountLink.addEventListener('click',this.openForm.bind(this));
         this.cancelBtn.addEventListener('click',this.cancelBtnHandler.bind(this));
         this.oldAccountLink.addEventListener('click',this.forgotPass.bind(this));
+        this.btnEmail.addEventListener('click', this.oldUser.bind(this))
+        this.newAccountBtn.addEventListener('click', this.newAccount.bind(this))
+        this.nextBtn.addEventListener('click', this.getUserValue.bind(this));
     }
     cancelBtnHandler(){
         if(this.openFormSwitch === true){
@@ -33,8 +41,11 @@ class LogonForm extends LogInForm{
 
     openForm(){
         this.capturedFirstName.classList.toggle('hidden');
+        this.nextBtn.classList.toggle('hidden')
+        this.newAccountBtn.classList.toggle('hidden')
         this.capturedLastName.classList.toggle('hidden');
         this.enterEmail.classList.toggle('hidden');
+        this.reEmail.classList.toggle('hidden');
         this.rePass.classList.toggle('hidden');
         this.cancelBtn.classList.toggle('hidden');
         this.newAccountLink.classList.toggle('hidden');
@@ -44,7 +55,9 @@ class LogonForm extends LogInForm{
     
     forgotPass(){
         this.capturedUsername.classList.toggle('hidden')
+        this.enterEmailBtn.classList.toggle('hidden')
         this.cancelBtn.classList.toggle('hidden');
+        this.nextBtn.classList.toggle('hidden')
         this.capturedPassword.classList.toggle('hidden');
         this.newAccountLink.classList.toggle('hidden');
         this.oldAccountLink.classList.toggle('hidden');
